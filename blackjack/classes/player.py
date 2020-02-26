8386b7ce4352c1a9326fc0a1060d19f6db2dca18
ellisandrews <ellisandrews1@gmail.com>
2020-02-26 16:18:53 -0600

    Refactor player class into parent and child classes

--- blackjack/classes/player.py
+++ blackjack/classes/player.py
@@ -5,23 +5,15 @@ class Player:
 
     all_ = []
 
-    def __init__(self, name, bankroll):
+    def __init__(self, name, table=None):
         self.name = name
-        self.bankroll = bankroll
+        self.table = table
 
     def __str__(self):
-        return f"Player: {self.name} | Bankroll: ${self.bankroll}"
-
+        return f"Player: {self.name}"
+    
     def hands(self):
         return [hand for hand in Hand.all_ if hand.player == self]
-
-    def add_bankroll(self, amount):
-        self.bankroll += amount
-
-    def subtract_bankroll(self, amount):
-        self.bankroll -= amount
-        if self.bankroll < 0:  # The player cannot have a negative bankroll
-            self.bankroll = 0
 
     def print_hands(self):
         for i, hand in enumerate(self.hands()):
@@ -33,3 +25,28 @@ class Player:
                 print(f"Hand {i+1} (${hand.wager}): {hand} -- ({low_total} or {high_total})")
             else:
                 print(f"Hand {i+1} (${hand.wager}): {hand} -- ({low_total})")
+
+
+class Gambler(Player):
+    
+    def __init__(self, name, bankroll=0, table=None, standing_wager=0):
+        super().__init__(self, name, table)
+        self.bankroll = bankroll
+        self.standing_wager = standing_wager
+
+    def __str__(self):
+        return super().__str__(self) + f" | Bankroll: ${self.bankroll}"
+
+    def add_bankroll(self, amount):
+        self.bankroll += amount
+
+    def subtract_bankroll(self, amount):
+        self.bankroll -= amount
+        if self.bankroll < 0:  # The player cannot have a negative bankroll
+            self.bankroll = 0
+
+
+class Dealer(Player):
+    
+    def __init__(self, name='Dealer', table=None):
+        super().__init__(self, name, table)

98f7c489de33d8a79964b839c79ba69faa213221
ellisandrews <ellisandrews1@gmail.com>
2020-06-26 14:49:09 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='Blackjack',
+    version='1.0',
+    description='Blackjack CLI interactive game and simulator.',
+    author='Ellis Andrews',
+    packages=['blackjack'],
+    install_requires=['matplotlib', 'pandas']
+)
